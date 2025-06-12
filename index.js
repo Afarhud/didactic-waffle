e8d30b07f71617b90433a726a09e16b36c569c97
sefidprog <sefid.dp@gmail.com>
2025-06-12 04:28:37 -0800

    تااهت

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ const { Client, Databases } = require('node-appwrite');
 
 module.exports = async ({ req, res, log, error }) => {
   try {
-    // Initialize Appwrite Client
     const client = new Client()
       .setEndpoint('https://cloud.appwrite.io/v1')
       .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
       .setKey(process.env.APPWRITE_API_KEY);
 
-    // Save execution time to DB
     const databases = new Databases(client);
     await databases.createDocument(
       '684aab9a0030ea5ac942',
@@ -20,25 +18,17 @@ module.exports = async ({ req, res, log, error }) => {
       }
     );
 
-    // Return success response with CORS headers
+    // Simple response
     return res.json({
       success: true,
       redirectUrl: 'https://anten.ir'
-    }, 200, {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'POST'
     });
 
   } catch (err) {
-    // Log error properly
     error(`Error: ${err.message}`);
-    
-    // Return error response with CORS headers
     return res.json({
       success: false,
       error: err.message
-    }, 500, {
-      'Access-Control-Allow-Origin': '*'
-    });
+    }, 500);
   }
 };
